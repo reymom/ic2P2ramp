23ae9a3836a365ac275a8fc260b810186ea081b6
reymom <ramon_gs95@hotmail.com>
2024-09-04 00:27:26 +0200

    clean evm vault code

--- backend/src/evm/vault.rs
+++ backend/src/evm/vault.rs
@@ -1,5 +1,3 @@
-use std::time::Duration;
-
 use ethers_core::types::{Address, U256};
 
 use super::fees::{self, FeeEstimates};
@@ -8,177 +6,13 @@ use super::{helpers, transaction};
 
 use crate::{
     errors::{RampError, Result},
-    state::{mutate_state, storage},
+    state::storage,
     types::{self, order::OrderState},
 };
 
 pub struct Ic2P2ramp;
 
 impl Ic2P2ramp {
-    pub async fn approve_token_allowance(
-        chain_id: u64,
-        token_address: &str,
-        gas: u32,
-    ) -> Result<()> {
-        helpers::validate_evm_address(&token_address)?;
-        if types::token_is_approved(chain_id, token_address)? {
-            return Err(RampError::TokenAlreadyRegistered);
-        };
-
-        let fee_estimates = fees::get_fee_estimates(9, chain_id).await;
-        let tx_hash = Self::approve_infinite_allowance(
-            chain_id,
-            token_address,
-            U256::from(gas),
-            fee_estimates,
-        )
-        .await?;
-
-        match transaction::wait_for_transaction_confirmation(
-            tx_hash.clone(),
-            chain_id,
-            60,
-            Duration::from_secs(4),
-        )
-        .await
-        {
-            Ok(_) => {
-                mutate_state(|state| {
-                    if let Some(chain_state) = state.chains.get_mut(&chain_id) {
-                        chain_state
-                            .approved_tokens
-                            .insert(token_address.to_string(), true);
-                    }
-                });
-                ic_cdk::println!(
-                    "[approve_token_allowance] Added token: {} for chain_id: {}",
-                    token_address,
-                    chain_id
-                );
-                Ok(())
-            }
-            Err(err) => Err(err),
-        }
-    }
-
-    pub async fn deposit_funds(
-        chain_id: u64,
-        amount: u128,
-        token_address: Option<String>,
-        gas: Option<u32>,
-    ) -> Result<String> {
-        let gas = U256::from(gas.unwrap_or(21_000));
-
-        let fee_estimates = fees::get_fee_estimates(9, chain_id).await;
-        ic_cdk::println!(
-            "[deposit_funds] gas = {:?}, max_fee_per_gas = {:?}, max_priority_fee_per_gas = {:?}",
-            gas,
-            fee_estimates.max_fee_per_gas,
-            fee_estimates.max_priority_fee_per_gas
-        );
-
-        let vault_manager_address = types::get_vault_manager_address(chain_id)?;
-
-        let request: SignRequest;
-        if let Some(token_address) = token_address {
-            if !types::token_is_approved(chain_id, &token_address)? {
-                return Err(RampError::TokenUnregistered);
-            }
-
-            request = Self::sign_request_deposit_token(
-                gas,
-                fee_estimates,
-                chain_id,
-                amount,
-                token_address,
-                vault_manager_address,
-            )
-            .await?;
-        } else {
-            request = Self::sign_request_deposit_base_currency(
-                gas,
-                fee_estimates,
-                chain_id,
-                amount,
-                vault_manager_address,
-            )
-            .await?;
-        }
-
-        transaction::send_signed_transaction(request, chain_id).await
-    }
-
-    async fn sign_request_deposit_token(
-        gas: U256,
-        fee_estimates: FeeEstimates,
-        chain_id: u64,
-        amount: u128,
-        token_address: String,
-        vault_manager_address: String,
-    ) -> Result<SignRequest> {
-        let abi = r#"
-            [
-                {
-                    "inputs": [
-                        {"internalType": "address", "name": "_token", "type": "address"},
-                        {"internalType": "uint256", "name": "_amount", "type": "uint256"}
-                    ],
-                    "name": "depositToken",
-                    "outputs": [],
-                    "stateMutability": "nonpayable",
-                    "type": "function"
-                }
-            ]
-        "#;
-
-        transaction::create_sign_request(
-            abi,
-            "depositToken",
-            gas,
-            fee_estimates,
-            chain_id,
-            U256::from(0),
-            vault_manager_address,
-            &[
-                ethers_core::abi::Token::Address(helpers::parse_address(token_address)?),
-                ethers_core::abi::Token::Uint(ethers_core::types::U256::from(amount)),
-            ],
-        )
-        .await
-    }
-
-    async fn sign_request_deposit_base_currency(
-        gas: U256,
-        fee_estimates: FeeEstimates,
-        chain_id: u64,
-        amount: u128,
-        vault_manager_address: String,
-    ) -> Result<SignRequest> {
-        let abi = r#"
-            [
-                {
-                    "inputs": [],
-                    "name": "depositBaseCurrency",
-                    "outputs": [],
-                    "stateMutability": "payable",
-                    "type": "function"
-                }
-            ]
-        "#;
-
-        transaction::create_sign_request(
-            abi,
-            "depositBaseCurrency",
-            gas,
-            fee_estimates,
-            chain_id,
-            U256::from(amount),
-            vault_manager_address,
-            &[],
-        )
-        .await
-    }
-
     pub async fn commit_deposit(
         chain_id: u64,
         offramper_address: String,
@@ -464,49 +298,6 @@ impl Ic2P2ramp {
             ],
         )
         .await
-    }
-
-    async fn approve_infinite_allowance(
-        chain_id: u64,
-        token_address: &str,
-        gas: U256,
-        fee_estimates: FeeEstimates,
-    ) -> Result<String> {
-        let abi = r#"
-            [
-                {
-                    "constant": false,
-                    "inputs": [
-                        {"name": "spender", "type": "address"},
-                        {"name": "value", "type": "uint256"}
-                    ],
-                    "name": "approve",
-                    "outputs": [{"name": "success", "type": "bool"}],
-                    "type": "function"
-                }
-            ]
-        "#;
-
-        let vault_manager_address = types::get_vault_manager_address(chain_id)?
-            .parse()
-            .map_err(|e| RampError::EthersAbiError(format!("Invalid address error: {:?}", e)))?;
-
-        let request = transaction::create_sign_request(
-            abi,
-            "approve",
-            gas,
-            fee_estimates,
-            chain_id,
-            U256::from(0),
-            token_address.to_string(),
-            &[
-                ethers_core::abi::Token::Address(vault_manager_address),
-                ethers_core::abi::Token::Uint(U256::max_value()),
-            ],
-        )
-        .await?;
-
-        transaction::send_signed_transaction(request, chain_id).await
     }
 
     pub async fn transfer_eth(
