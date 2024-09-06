fdb90139fc5b5be87ea3a8288c4497540eeaabc3
reymom <ramon_gs95@hotmail.com>
2024-09-06 21:08:23 +0200

    [front] app layout style

--- src/App.tsx
+++ src/App.tsx
@@ -51,8 +51,8 @@ function App() {
     return (
         <div className="min-h-screen bg-gray-50">
             <Menu />
-            <div className="flex flex-col items-center mt-8">
-                <div className="bg-white p-8 space-x-2 space-y-2 rounded shadow-md text-center w-full sm:w-3/4 md:w-1/2 lg:w-5/12 xl:w-1/3">
+            <div className="flex-grow flex justify-center items-center py-8">
+                <div className="text-center w-full sm:w-3/4 md:w-1/2 lg:w-5/12 xl:w-1/3">
                     <Routes>
                         <Route path="/" element={<ConnectAddress />} />
                         <Route path="/register" element={<RegisterUser />} />
