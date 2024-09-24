82c060e144178a5ef358ed1c8d1053ad4b3e9c01
reymom <ramon_gs95@hotmail.com>
2024-09-24 22:44:36 +0200

    sandbox and production deployment configurations

--- vite.config.ts
+++ vite.config.ts
@@ -4,66 +4,71 @@ import { defineConfig } from 'vite';
 import environment from 'vite-plugin-environment';
 import dotenv from 'dotenv';
 import path from 'path';
-import { resolve } from 'path';
 
-dotenv.config();
+// dotenv.config();
 
-export default defineConfig({
-  root: 'src',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis',
+export default defineConfig(({ mode }) => {
+  dotenv.config({ path: `.env.${mode}` });
+
+  const domain = mode === 'test' ? 'sandbox.icramp.xyz' : 'app.icramp.xyz';
+
+  return {
+    root: 'src',
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        define: {
+          global: 'globalThis',
+        },
       },
     },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:4943',
-        changeOrigin: true,
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://127.0.0.1:4943',
+          changeOrigin: true,
+        },
       },
     },
-  },
-  plugins: [
-    react(),
-    environment('all', { prefix: 'CANISTER_' }),
-    environment('all', { prefix: 'DFX_' }),
-    environment('all', { prefix: 'CONTRACT_' }),
-    environment('all', { prefix: 'FRONTEND_' }),
-    {
-      name: 'copy-assets',
-      generateBundle() {
-        this.emitFile({
-          type: 'asset',
-          fileName: '.well-known/ic-domains',
-          source: 'app.ic2p2ramp.xyz',
-        });
-        this.emitFile({
-          type: 'asset',
-          fileName: '.ic-assets.json',
-          source: '[{"match": ".well-known", "ignore": false}]',
-        });
+    plugins: [
+      react(),
+      environment('all', { prefix: 'CANISTER_' }),
+      environment('all', { prefix: 'DFX_' }),
+      environment('all', { prefix: 'CONTRACT_' }),
+      environment('all', { prefix: 'FRONTEND_' }),
+      {
+        name: 'copy-assets',
+        generateBundle() {
+          this.emitFile({
+            type: 'asset',
+            fileName: '.well-known/ic-domains',
+            source: domain,
+          });
+          this.emitFile({
+            type: 'asset',
+            fileName: '.ic-assets.json',
+            source: '[{"match": ".well-known", "ignore": false}]',
+          });
+        },
+      },
+    ],
+    test: {
+      environment: 'jsdom',
+      setupFiles: 'setupTests.ts',
+      cache: { dir: '../node_modules/.vitest' },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "${path.resolve(
+            __dirname,
+            'src/index.css',
+          )}";`,
+        },
       },
     },
-  ],
-  test: {
-    environment: 'jsdom',
-    setupFiles: 'setupTests.ts',
-    cache: { dir: '../node_modules/.vitest' },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "${path.resolve(
-          __dirname,
-          'src/index.css',
-        )}";`,
-      },
-    },
-  },
+  };
 });
