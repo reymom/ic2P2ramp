7c5c91822e231e35bdfaeea5e1663c3898dbe06e
reymom <ramon_gs95@hotmail.com>
2025-01-04 09:01:49 +0100

    bitcoin_backend: wallet config param

--- bitcoin_backend/src/model/types/wallet.rs
+++ bitcoin_backend/src/model/types/wallet.rs
@@ -1,7 +1,53 @@
-use candid::{CandidType, Deserialize};
+use ic_cdk::api::management_canister::bitcoin::BitcoinNetwork;
 
-#[derive(CandidType, Deserialize)]
-pub struct SendRequest {
-    pub destination_address: String,
-    pub amount_in_satoshi: u64,
+use crate::memory::heap::config::{DERIVATION_PATH, KEY_NAME, NETWORK};
+
+pub enum TaprootUseCase {
+    Standard,
+    RuneTransfer(String),
+    Inscription(Vec<u8>),
+}
+
+#[derive(Clone)]
+pub struct WalletConfig {
+    pub key_name: String,
+    pub network: BitcoinNetwork,
+    pub derivation_path: Vec<Vec<u8>>,
+}
+
+pub enum AddressType {
+    P2PKH,
+    P2TRRawKey,
+    P2TRScript,
+}
+
+impl WalletConfig {
+    fn new(address_type: AddressType) -> Self {
+        let mut derivation_path = DERIVATION_PATH.with(|d| d.clone());
+        match address_type {
+            AddressType::P2PKH => (),
+            AddressType::P2TRRawKey => derivation_path.push(b"key_spend".to_vec()),
+            AddressType::P2TRScript => {
+                derivation_path.push(b"script_spend".to_vec());
+            }
+        }
+
+        Self {
+            key_name: KEY_NAME.with(|kn| kn.borrow().to_string()),
+            network: NETWORK.with(|n| n.get()),
+            derivation_path,
+        }
+    }
+
+    pub fn for_p2pkh() -> Self {
+        Self::new(AddressType::P2PKH)
+    }
+
+    pub fn for_p2tr_raw_key() -> Self {
+        Self::new(AddressType::P2TRRawKey)
+    }
+
+    pub fn for_p2tr_script() -> Self {
+        Self::new(AddressType::P2TRScript)
+    }
 }
