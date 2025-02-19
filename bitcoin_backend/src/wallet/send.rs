41238a5fd77a9b0c712db1350f3aa8daa56cfa8e
reymom <ramon_gs95@hotmail.com>
2025-02-19 08:23:53 +0100

    wallet send with new tx options

--- bitcoin_backend/src/wallet/send.rs
+++ bitcoin_backend/src/wallet/send.rs
@@ -2,15 +2,11 @@ use bitcoin::Txid;
 use ic_cdk::api::management_canister::bitcoin::Satoshi;
 
 use crate::{
-    memory::heap::config,
-    model::types::{
-        errors::Result,
-        transfer::TransactionType,
-        wallet::{TaprootUseCase, WalletConfig},
-    },
+    errors::BitcoinError,
+    model::types::{errors::Result, transfer::TransactionType, wallet::WalletConfig},
 };
 
-pub async fn send_btc_or_rune(
+pub async fn send_btc_or_ordinal(
     dst_address: String,
     amount: Satoshi,
     tx_type: TransactionType,
@@ -19,35 +15,19 @@ pub async fn send_btc_or_rune(
         TransactionType::LegacyBitcoin => {
             crate::wallet::p2pkh::send(WalletConfig::for_p2pkh(), dst_address, amount).await
         }
-        TransactionType::SimpleTaprootBitcoin => {
+        TransactionType::TaprootBitcoin
+        | TransactionType::RuneTransfer(_)
+        | TransactionType::OrdinalTransfer => {
             crate::wallet::p2tr_raw_key_spend::send_key_spend(
                 WalletConfig::for_p2tr_raw_key(),
                 dst_address,
                 amount,
-            )
-            .await
-        }
-        TransactionType::ScriptedTaprootBitcoin => {
-            crate::wallet::p2tr_script_spend::send_script_spend(
-                WalletConfig::for_p2tr_script(),
-                TaprootUseCase::Standard,
-                dst_address,
-                amount,
                 tx_type,
             )
             .await
         }
-        TransactionType::RuneTransfer(ref rune) => {
-            let rune_symbol = config::get_rune_metadata(&rune)?.symbol;
-            ic_cdk::println!("[send] rune_symbol: {}", rune_symbol);
-            crate::wallet::p2tr_script_spend::send_script_spend(
-                WalletConfig::for_p2tr_script(),
-                TaprootUseCase::RuneTransfer(rune_symbol),
-                dst_address,
-                amount,
-                tx_type,
-            )
-            .await
+        TransactionType::RuneEtching(_) | TransactionType::OrdinalInscription(_) => {
+            Err(BitcoinError::InternalError("not available yet".to_string()))
         }
     }
 }
