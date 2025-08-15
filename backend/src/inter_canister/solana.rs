6acc723abab074df8f1f57bdd68722881e9e481a
reymom <ramon_gs95@hotmail.com>
2025-08-15 23:17:23 +0200

    backend: scaffold solana inter canister calls

---
+++ backend/src/inter_canister/solana.rs
@@ -0,0 +1,46 @@
+use candid::{Nat, Principal};
+use ic_cdk::call;
+
+use crate::model::errors::{Result, SystemError};
+
+const SOLANA_BACKEND_CANISTER_ID: &str = "u6s2n-gx777-77774-qaaba-cai";
+
+pub async fn solana_backend_send_sol(dst: String, lamports: Nat) -> Result<String> {
+    let solana_backend_canister_id = Principal::from_text(SOLANA_BACKEND_CANISTER_ID)
+        .map_err(|_| SystemError::InvalidInput("Invalid ledger principal".to_string()))?;
+
+    let (tx_id,): (Result<String>,) =
+        (call::<(Option<Principal>, String, Nat), (Result<String>,)>(
+            solana_backend_canister_id,
+            "send_sol",
+            (Some(ic_cdk::id()), dst, amount),
+        )
+        .await
+        .map_err(|(code, err)| SystemError::ICRejectionError(code, err))?
+        .0,);
+
+    Ok(tx_id?)
+}
+
+pub async fn solana_backend_send_spl_token(
+    mint_account: String,
+    to: String,
+    amount: Nat,
+) -> Result<String> {
+    let solana_backend_canister_id = Principal::from_text(SOLANA_BACKEND_CANISTER_ID)
+        .map_err(|_| SystemError::InvalidInput("Invalid ledger principal".to_string()))?;
+
+    let (tx_id,): (Result<String>,) = (call::<
+        (Option<Principal>, String, String, Nat),
+        (Result<String>,),
+    >(
+        solana_backend_canister_id,
+        "send_spl_token",
+        (Some(ic_cdk::id()), mint_account, to, amount),
+    )
+    .await
+    .map_err(|(code, err)| SystemError::ICRejectionError(code, err))?
+    .0,);
+
+    Ok(tx_id?)
+}
