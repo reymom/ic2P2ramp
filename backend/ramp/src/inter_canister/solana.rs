6bad6fee23e1eeafb0ce6e4c73902b371fc65573
reymom <ramon_gs95@hotmail.com>
2025-08-16 01:28:51 +0200

    ramp: implement solana vault intercalls

--- backend/ramp/src/inter_canister/solana.rs
+++ backend/ramp/src/inter_canister/solana.rs
@@ -47,3 +47,100 @@ pub async fn solana_backend_send_spl_token(
 
     Ok(tx_id?)
 }
+
+pub async fn solana_backend_deposit_funds(
+    offramper: String,
+    amount: u64,
+    token_mint: Option<String>,
+) -> Result<()> {
+    let can_id = Principal::from_text(SOLANA_BACKEND_CANISTER_ID)
+        .map_err(|_| SystemError::InvalidInput("Invalid solana_backend principal".to_string()))?;
+
+    let (res,): (SolanaResult<()>,) = call::<(String, u64, Option<String>), _>(
+        can_id,
+        "deposit_to_vault_canister",
+        (offramper, amount, token_mint),
+    )
+    .await
+    .map_err(|(code, err)| SystemError::ICRejectionError(code, err))?;
+
+    Ok(res?)
+}
+
+pub async fn solana_backend_cancel_deposit(
+    offramper: String,
+    amount: u64,
+    token_mint: Option<String>,
+) -> Result<()> {
+    let can_id = Principal::from_text(SOLANA_BACKEND_CANISTER_ID)
+        .map_err(|_| SystemError::InvalidInput("Invalid solana_backend principal".to_string()))?;
+
+    let (res,): (SolanaResult<()>,) = call::<(String, u64, Option<String>), _>(
+        can_id,
+        "cancel_deposit",
+        (offramper, amount, token_mint),
+    )
+    .await
+    .map_err(|(code, err)| SystemError::ICRejectionError(code, err))?;
+
+    Ok(res?)
+}
+
+pub async fn solana_backend_lock_funds(
+    offramper: String,
+    onramper: String,
+    amount: u64,
+    token_mint: Option<String>,
+) -> Result<()> {
+    let can_id = Principal::from_text(SOLANA_BACKEND_CANISTER_ID)
+        .map_err(|_| SystemError::InvalidInput("Invalid solana_backend principal".to_string()))?;
+
+    let (res,): (SolanaResult<()>,) = call::<(String, String, u64, Option<String>), _>(
+        can_id,
+        "lock_funds",
+        (offramper, onramper, amount, token_mint),
+    )
+    .await
+    .map_err(|(code, err)| SystemError::ICRejectionError(code, err))?;
+
+    Ok(res?)
+}
+
+pub async fn solana_backend_unlock_funds(
+    offramper: String,
+    onramper: String,
+    amount: u64,
+    token_mint: Option<String>,
+) -> Result<()> {
+    let can_id = Principal::from_text(SOLANA_BACKEND_CANISTER_ID)
+        .map_err(|_| SystemError::InvalidInput("Invalid solana_backend principal".to_string()))?;
+
+    let (res,): (SolanaResult<()>,) = call::<(String, String, u64, Option<String>), _>(
+        can_id,
+        "unlock_funds",
+        (offramper, onramper, amount, token_mint),
+    )
+    .await
+    .map_err(|(code, err)| SystemError::ICRejectionError(code, err))?;
+
+    Ok(res?)
+}
+
+pub async fn solana_backend_complete_order(
+    onramper_address: String,
+    amount: u64,
+    token_mint: Option<String>,
+) -> Result<()> {
+    let can_id = Principal::from_text(SOLANA_BACKEND_CANISTER_ID)
+        .map_err(|_| SystemError::InvalidInput("Invalid solana_backend principal".to_string()))?;
+
+    let (res,): (SolanaResult<()>,) = call::<(String, u64, Option<String>), _>(
+        can_id,
+        "complete_order",
+        (onramper_address, amount, token_mint),
+    )
+    .await
+    .map_err(|(code, err)| SystemError::ICRejectionError(code, err))?;
+
+    Ok(res?)
+}
