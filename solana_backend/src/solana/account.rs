6d68608dffaa01345ba94ed8898ce62416ea1ccb
reymom <ramon_gs95@hotmail.com>
2025-08-15 16:24:09 +0200

    solana_backend: add cycles to account derivation

--- solana_backend/src/solana/account.rs
+++ solana_backend/src/solana/account.rs
@@ -2,17 +2,21 @@ use sol_rpc_types::GetAccountInfoEncoding;
 use solana_pubkey::Pubkey;
 use std::str::FromStr;
 
-use crate::solana::client::client;
+use crate::{
+    model::types::errors::{Result, SolanaError},
+    solana::client::client,
+};
 
-pub async fn get_account_owner(account: &Pubkey) -> Pubkey {
+pub async fn get_account_owner(account: &Pubkey) -> Result<Pubkey> {
     let owner = client()
         .get_account_info(*account)
         .with_encoding(GetAccountInfoEncoding::Base64)
+        .with_cycles(3_000_000_000)
         .send()
         .await
         .expect_consistent()
-        .expect("Call to `getAccountInfo` failed")
-        .unwrap_or_else(|| panic!("Account not found for pubkey `{account}`"))
+        .map_err(SolanaError::from)?
+        .ok_or_else(|| SolanaError::AccountNotFound)?
         .owner;
-    Pubkey::from_str(&owner).unwrap()
+    Pubkey::from_str(&owner).map_err(SolanaError::from)
 }
